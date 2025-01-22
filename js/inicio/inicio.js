1b95e3a65cb7d81c38f34a7d8e8cb98a4903e499
VicenteVillena <VicenteVillena2007@gmail.com>
2025-01-22 08:35:27 -0500

    Update inicio.js

--- js/inicio/inicio.js
+++ js/inicio/inicio.js
@@ -1,7 +1,36 @@
+window.onload = function(){
+    //Todo lo que este aca, se va a ejecutar cuando ka pagina este cargada
 
+    mostrarMensaje();
+}
 
-function mostrarMensaje(){
+//Variables
+const nombre = "Vicente";
+let  apellido = "Villena";
+var dirrecion = "238.23.107.120"
+var edad = 30;
 
-    console.log("Hola Mundo Att Vicente");
+/*TypeScript
+String nombrev2 = "";
+Number edad = "69";*/
 
+let Alumno = {
+    nombre:"Cesaer",
+    apellido: "Manidas",
+    edad: 20,
+    dirrecion: "Jr"
+}
+
+let Alumn2 = {
+    nombre:"Vicente",
+    apellido: "Villena",
+    edad: 19,
+    dirrecion: "Jr"
+}
+
+let arregloAlumno = [Alumno, Alumn2];
+let ArregloNumeros = [1,2,3,4]
+
+function mostrarMensaje(valor){
+    alert("Hola "+valor)
 }
