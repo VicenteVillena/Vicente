1c2a48060ae1caa52747695b10c998e18f34f825
VicenteVillena <VicenteVillena2007@gmail.com>
2025-01-27 09:56:05 -0500

    seccion 4

--- js/inicio/Contactanos.js
+++ js/inicio/Contactanos.js
@@ -1,10 +1,8 @@
-
 window.onload = function(){
     //Todo lo que este acá se va a ejecutar cuando la 
     //pagina este cargada
 
     $("#idEnviar").attr("disabled", true);
-    mostrarMensaje();
 
     $("#idCheckbox").on("change", function(){   
         let checked = v_confirmacion = $(this).prop("checked");
@@ -16,14 +14,21 @@ window.onload = function(){
     });
 
     $("#idEnviar").on("click", function(){
-        enviarDatos();
+        enviardatos();
     });
 }
 
-function enviarDatos(){
-    v_nombre = $("#idNombre").val();
-    v_correo = $("#idEmail").val();
-    v_contrasenia = $("#idPassword").val();
+$("#idEnviar").on("click", function(){
+
+    $("#idModal").modal('show');
+
+});
+
+function enviardatos(){
+    v_nombre = $("#idNomnbre").val();
+    v_correo = $("#idGmail").val();
+    v_numero = $("#idNT").val();
+    debugger
 
     var regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
     let resultado = regex.test(v_correo);
@@ -31,47 +36,26 @@ function enviarDatos(){
     if (v_confirmacion && 
         v_nombre != null && v_nombre.trim().length > 0 &&
         v_correo != null && v_correo.trim().length > 0 && resultado &&
-        v_contrasenia != null && v_contrasenia.trim().length > 0
+        v_numero != null && v_numero.trim().length > 0
     ) {
-        $("#idNavbar").text(v_nombre);
+
+        enviardatosM();
+        
     }else{
-        $("#idNavbar").text("Navbar");
         alert("Ingrese datos válidos");
     }
 
 }
 
-
 //Variables
 let v_confirmacion = false;
 let v_nombre = null;
 let v_correo = null;
-let v_contrasenia = null;
+let v_numero = null;
 
 
-const nombre = "César";
-let apellido = "Mantilla";
-var direccion = "Jr. Direccion 123";
-var edad = 30;
-
-let alumno1 = {
-    nombre:"César",
-    apellido: "Mantilla",
-    edad: 20,
-    direccion: "Jr. Direccion 123"
+function enviardatosM(){
+    console.log("Cerrando modal");
+    $("#idModal").modal('hide');
+    window.location.href="../index.html";
 }
-let alumno2 = {
-    nombre:"Vicente",
-    apellido: "Villena",
-    edad: 19,
-    direccion: "Jr. Direccion 456"
-}
-
-let arregloAlumno = [alumno1, alumno2];
-let arregloNumeros = [1, 2,3 , 4];
-let arregloX = [1, 2,3 , 4, "Hola", alumno1];
-
-function mostrarMensaje(valor){
-    console.log("Bienvenidos !!")
-}
-
