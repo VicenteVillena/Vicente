0439e4e38e49dc1abebb9e4c2ef0dea934751697
VicenteVillena <VicenteVillena2007@gmail.com>
2025-02-20 07:57:39 -0500

    Add files via upload

--- js/inicio/Proyecto.js
+++ js/inicio/Proyecto.js
@@ -1,167 +1,110 @@
-$(document).ready(function(){
+$(document).ready(function () {
   getItems();
 
   proyectosTemporales = listaDeProyecytos
+  generarProyectos();
+
+  obtenerParam();
+
+  $("#idValor").on("search", function () {
+
+    proyectosTemporales = listaDeProyecytos;
     generarProyectos();
-
-    obtenerParam();
-
-    $("#idValor").on("search", function(){
-      
-      proyectosTemporales = listaDeProyecytos;
-      generarProyectos();
-    });
+  });
 
 });
 
 let proyectosTemporales = [];
 
-let listaDeProyecytos = [
-    {
-        ipProyecto: 1,
-        titulo:"K-on el videojuego",
-        discripcion:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        fecha: "01/10/2024",
-        tecnologia: ["html", "JavaScript", "Azure", "Sql Server"],
-        etiquetas: ["Juegos", "Online"],
-        imagenes: ["../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png"
-        ]
-    },
-    {
-        ipProyecto: 2,
-        titulo:"Segundo Proyecto",
-        discripcion:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        fecha: "01/10/2024",
-        tecnologia: ["html", "JavaScript", "Azure", "Sql Server"],
-        etiquetas: ["Juegos", "Online"],
-        imagenes: ["../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png"
-        ]
-    },
-    {
-        ipProyecto: 3,
-        titulo:"Tercer Proyecto",
-        discripcion:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        fecha: "01/10/2024",
-        tecnologia: ["html", "JavaScript", "Azure", "Sql Server"],
-        etiquetas: ["Juegos", "Online"],
-        imagenes: ["../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png"
-        ]
-    },
-    {
-        ipProyecto: 4,
-        titulo:"Cuarto Proyecto",
-        discripcion:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        fecha: "01/10/2024",
-        tecnologia: ["html", "JavaScript", "Azure", "Sql Server"],
-        etiquetas: ["Juegos", "Online"],
-        imagenes: ["../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png",
-          "../img/carousel/K-ON.png"
-        ]
-    },
-];
+let listaDeProyecytos = [];
 
-$("#idBuscar").on("click", function(){
+$("#idBuscar").on("click", function () {
 
-  buscarProyectos ($("#idValor").val());
+  buscarProyectos($("#idValor").val());
 });
 
-function buscarProyectos(val){
+function buscarProyectos(val) {
   proyectosTemporales = listaDeProyecytos;
-  proyectosTemporales = proyectosTemporales.filter(x=> x.titulo.includes(val));
+  proyectosTemporales = proyectosTemporales.filter(x => x.titulo.includes(val));
 
   generarProyectos();
-};
+}
 
-function generarTags(arr){
+function generarTags(arr) {
   let resultado = "";
 
   if (arr != undefined && arr != null && arr.length > 0) {
     arr.forEach(e => {
-      resultado += '<span class="badge text-bg-success">'+e+'</span>';
+      resultado += '<span class="badge text-bg-success">' + e + '</span>';
     });
   }
-
   return resultado;
 }
 
-function generarProyectos(){
-    $(".alingItem").empty();
+function generarProyectos() {
+  debugger
+  $(".alingItem").empty();
 
-    for(let i = 0; i < proyectosTemporales.length; i++) {
-        const proyecto = proyectosTemporales[i];
+  for (let i = 0; i < proyectosTemporales.length; i++) {
+    const proyecto = proyectosTemporales[i];
 
-      let itemsCarusel = "";
+    let itemsCarusel = "";
 
-      if (proyecto.imagenes.length >0){
+    if (proyecto.imagenes.length > 0) {
 
-        for (let i = 0; i < proyecto.imagenes.length; i++){
-          if (i == 0){
-            itemsCarusel += '<div class="carousel-item active">'+
-                            '<img src="'+proyecto.imagenes[i]+'" class="d-block w-100" alt="...">'+
-                          '</div>';
-          }else{
-            itemsCarusel += '<div class="carousel-item">'+
-                              '<img src="'+proyecto.imagenes[i]+'" class="d-block w-100" alt="...">'+
-                            '</div>';
-          }
+      for (let i = 0; i < proyecto.imagenes.length; i++) {
+        if (i == 0) {
+          itemsCarusel += '<div class="carousel-item active">' +
+            '<img src="' + proyecto.imagenes[i] + '" class="d-block w-100" alt="...">' +
+            '</div>';
+        } else {
+          itemsCarusel += '<div class="carousel-item">' +
+            '<img src="' + proyecto.imagenes[i] + '" class="d-block w-100" alt="...">' +
+            '</div>';
         }
-
-      }else{
-        itemsCarusel = '<div class="carousel-item">'+
-                         ' <img src="..." class="d-block w-100" alt="...">'+
-                        '</div>';
       }
 
-        let item = ' <div class="card mb-3" style="max-width: 440px;">'+
-        '<div class="row g-0">'+
-          '<div class="col-md-4">'+
-          '<div id="carouselExample" class="carousel slide">'+
-          '<div class="carousel-inner">'+itemsCarusel+'</div>'+
-          '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">'+
-            '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'+
-            '<span class="visually-hidden">Previous</span>'+
-          '</button>'+
-          '<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">'+
-            '<span class="carousel-control-next-icon" aria-hidden="true"></span>'+
-            '<span class="visually-hidden">Next</span>'+
-          '</button>'+
-        '</div>'+
-          '</div>'+
-          '<div class="col-md-8">'+
-            '<div class="card-body">'+
-              '<h5 class="card-title">'+proyecto.titulo+'</h5>'+
-              '<p class="card-text">'+proyecto.discripcion+'</p>'+
-              '<p class="card-text"><small class="text-body-secondary">'+proyecto.fecha+'</small></p>'+
-              '<button onclick="vistaprevia('+proyecto.ipProyecto+')" id="preview" class="btn btn-sm btn-success" type="button">Ver Mas</button>'+
-              '<small class="btn btn-sm btn-primary" onclick="editarProyecto('+proyecto.ipProyecto+')" >Editar</small>'+
-              '<div>'+generarTags(proyecto.etiquetas)+'</div>'+
-            '</div>'+
-          '</div>'+
-        '</div>'+
+    } else {
+      itemsCarusel = '<div class="carousel-item">' +
+        ' <img src="..." class="d-block w-100" alt="...">' +
+        '</div>';
+    }
+
+    let item = ' <div class="card mb-3" style="max-width: 440px;">' +
+      '<div class="row g-0">' +
+      '<div class="col-md-4">' +
+      '<div id="carouselExample" class="carousel slide">' +
+      '<div class="carousel-inner">' + itemsCarusel + '</div>' +
+      '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">' +
+      '<span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
+      '<span class="visually-hidden">Previous</span>' +
+      '</button>' +
+      '<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">' +
+      '<span class="carousel-control-next-icon" aria-hidden="true"></span>' +
+      '<span class="visually-hidden">Next</span>' +
+      '</button>' +
+      '</div>' +
+      '</div>' +
+      '<div class="col-md-8">' +
+      '<div class="card-body">' +
+      '<h5 class="card-title">' + proyecto.titulo + '</h5>' +
+      '<p class="card-text">' + proyecto.descripcion + '</p>' +
+      '<p class="card-text"><small class="text-body-secondary">' + proyecto.fecha + '</small></p>' +
+      '<button onclick="vistaprevia(' + proyecto._id + ')" id="preview" class="btn btn-sm btn-success" type="button">Ver Mas</button>' +
+      '<button class="btn btn-sm btn-primary" onclick="editarProyectoV2(' + proyecto._id + ')" >Editar</button>' +
+      '<div>' + generarTags(proyecto.etiquetas) + '</div>' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
       '</div>';
 
     $(".alingItem").append(item)
 
-    }
+  }
 
-};
+}
 
-function ValidadCampo(){
+function ValidadCampo() {
 
   let valido = false;
 
@@ -169,33 +112,33 @@ function ValidadCampo(){
   let vDos = $("#idDescProyF").text();
   let vTres = $("#idFecProyF").val();
 
-  if (vUno != undefined && vUno != null && vUno.trim()!= "" &&
-      vDos != undefined && vDos != null && vDos.trim()!= "" &&
-      vTres != undefined && vTres != null && vTres.trim()!= "") {
-        valido = true
+  if (vUno != undefined && vUno != null && vUno.trim() != "" &&
+    vDos != undefined && vDos != null && vDos.trim() != "" &&
+    vTres != undefined && vTres != null && vTres.trim() != "") {
+    valido = true
   }
 
   return valido;
 }
 
-function actualizarProy(idItem){
-  
+function actualizarProy(idItem) {
 
 
-  if (ValidadCampo()){
 
-let item = {
-  "titulo": $("#idTituloProyF").val(),
-  "descripcion": $("#idDescProyF").text(),
-  "fecha": $("#idFecProyF").val(),
-  "tecnologias": $("#idTecProyF").val().split(";"),
-  "etiquetas": $("#idEtiqProyF").val().split(";"),
-  "imagenes": $("#idImgProyF").val().split(";")
-};
+  if (ValidadCampo()) {
+
+    let item = {
+      "titulo": $("#idTituloProyF").val(),
+      "descripcion": $("#idDescProyF").text(),
+      "fecha": $("#idFecProyF").val(),
+      "tecnologias": $("#idTecProyF").val().split(";"),
+      "etiquetas": $("#idEtiqProyF").val().split(";"),
+      "imagenes": $("#idImgProyF").val().split(";")
+    };
 
 
     var settings = {
-      "url": "http://localhost:9000/api/todos/"+idItem,
+      "url": "http://localhost:9000/api/todos/" + idItem,
       "method": "PUT",
       "timeout": 0,
       "headers": {
@@ -203,69 +146,30 @@ let item = {
       },
       "data": JSON.stringify(item),
     };
-    
+
     $.ajax(settings).done(function (response) {
       generarProyectos();
       $("#idModalForm").modal('hide');
     });
 
-    /*let indiceProy = listaDeProyecytos.findIndex(x=> x.ipProyecto == val);
-    listaDeProyecytos[indiceProy].titulo = $("#idTituloProyF").val();
-    listaDeProyecytos[indiceProy].discripcion = $("#idDescProyF").text();
-    listaDeProyecytos[indiceProy].fecha = $("#idFecProyF").val();
-  
-    listaDeProyecytos[indiceProy].tecnologia = [];
-    listaDeProyecytos[indiceProy].etiquetas = [];
-    listaDeProyecytos[indiceProy].imagenes = [];
-    let tec = $("#idTecProyF").val().split(";");
-    tec.forEach(e => {
-      listaDeProyecytos[indiceProy].tecnologia.push(e.trim());
-    });
-    let tag = $("#idEtiqProyF").val().split(";");
-    tag.forEach(e => {
-      listaDeProyecytos[indiceProy].etiquetas.push(e.trim());
-    });
-    let img = $("#idImgProyF").val().split(";");
-    img.forEach(e => {
-      listaDeProyecytos[indiceProy].imagenes.push(e.trim());
-    });
-  */
 
-  }else{
+  } else {
     $("#IdModalAdvert").modal('show');
   }
 }
 
-function editarProyecto(val){
-  
-   
+function editarProyectoV2(val) {
 
-    let proyecto = listaDeProyecytos.find(x=> x.ipProyecto == val);
-  
-    $("#idTituloForm").text("Editando "+proyecto.titulo);
-  
-    $("#idTituloProyF").val(proyecto.titulo);
-    $("#idDescProyF").text(proyecto.discripcion);
-    $("#idFecProyF").val(proyecto.fecha);
-  
-  
-    let tecnologia = proyecto.tecnologia.join(";");
-    let etiquetas = proyecto.etiquetas.join(";");
-    let imagenes = proyecto.imagenes.join(";");
-  
-    $("#idTecProyF").val(tecnologia);
-    $("#idEtiqProyF").val(etiquetas);
-    $("#idImgProyF").val(imagenes);
-    $("#idBtnGuardar").empty();
-    $("#idBtnGuardar").append('<a class="btn btn-primary" onclick="actualizarProy('+proyecto.ipProyecto+')">Guardar</a>');
-  
-    $("#idModalForm").modal('show');
-  
-  }
+  debugger
+  $("#idModalForm").modal('show');
 
-function obtenerParam(){
 
-  if(window.location.search !=''){
+
+}
+
+function obtenerParam() {
+
+  if (window.location.search != '') {
 
     let param = window.location.search
     param = param.split("=")[1];
@@ -275,70 +179,69 @@ function obtenerParam(){
   }
 }
 
-function vistaprevia(val){
-  let proyecto = listaDeProyecytos.find(x=> x.ipProyecto == val);
-debugger
+function vistaprevia(val) {
+  let proyecto = listaDeProyecytos.find(x => x.ipProyecto == val);
+  debugger
   $("#TituloCambio").text(proyecto.titulo);
-  $("#Desc").text(proyecto.discripcion);
+  $("#Desc").text(proyecto.descripcion);
 
-  
+
 
 
   let itemsCarusel = "";
 
-  if (proyecto.imagenes.length >0){
+  if (proyecto.imagenes.length > 0) {
 
-    for (let i = 0; i < proyecto.imagenes.length; i++){
-      if (i == 0){
-        itemsCarusel += '<div class="carousel-item active">'+
-                        '<img src="'+proyecto.imagenes[i]+'" class="d-block w-100" alt="...">'+
-                      '</div>';
-      }else{
-        itemsCarusel += '<div class="carousel-item">'+
-                          '<img src="'+proyecto.imagenes[i]+'" class="d-block w-100" alt="...">'+
-                        '</div>';
+    for (let i = 0; i < proyecto.imagenes.length; i++) {
+      if (i == 0) {
+        itemsCarusel += '<div class="carousel-item active">' +
+          '<img src="' + proyecto.imagenes[i] + '" class="d-block w-100" alt="...">' +
+          '</div>';
+      } else {
+        itemsCarusel += '<div class="carousel-item">' +
+          '<img src="' + proyecto.imagenes[i] + '" class="d-block w-100" alt="...">' +
+          '</div>';
       }
     }
 
-  }else{
-    itemsCarusel = '<div class="carousel-item">'+
-                     ' <img src="..." class="d-block w-100" alt="...">'+
-                    '</div>';
+  } else {
+    itemsCarusel = '<div class="carousel-item">' +
+      ' <img src="..." class="d-block w-100" alt="...">' +
+      '</div>';
   }
 
-    let item = ' <div class="card mb-3" style="max-width: 440px;">'+
-    '<div class="row g-0">'+
-      '<div class="col">'+
-      '<span class="badge text-bg-danger">'+proyecto.etiquetas[0]+'</span>'+
-      '<span class="badge text-bg-danger">'+proyecto.etiquetas[1]+'</span>'+
-      '<div id="carouselExample2'+proyecto.ipProyecto+'" class="carousel slide">'+
-      '<div class="carousel-inner">'+itemsCarusel+'</div>'+
-      '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample2'+proyecto.ipProyecto+'" data-bs-slide="prev">'+
-        '<span class="carousel-control-prev-icon" aria-hidden="true"></span>'+
-        '<span class="visually-hidden">Previous</span>'+
-      '</button>'+
-      '<button class="carousel-control-next" type="button" data-bs-target="#carouselExample2'+proyecto.ipProyecto+'" data-bs-slide="next">'+
-        '<span class="carousel-control-next-icon" aria-hidden="true"></span>'+
-        '<span class="visually-hidden">Next</span>'+
-      '</button>'+
+  let item = ' <div class="card mb-3" style="max-width: 440px;">' +
+    '<div class="row g-0">' +
+    '<div class="col">' +
+    '<span class="badge text-bg-danger">' + proyecto.etiquetas[0] + '</span>' +
+    '<span class="badge text-bg-danger">' + proyecto.etiquetas[1] + '</span>' +
+    '<div id="carouselExample2' + proyecto.ipProyecto + '" class="carousel slide">' +
+    '<div class="carousel-inner">' + itemsCarusel + '</div>' +
+    '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample2' + proyecto.ipProyecto + '" data-bs-slide="prev">' +
+    '<span class="carousel-control-prev-icon" aria-hidden="true"></span>' +
+    '<span class="visually-hidden">Previous</span>' +
+    '</button>' +
+    '<button class="carousel-control-next" type="button" data-bs-target="#carouselExample2' + proyecto.ipProyecto + '" data-bs-slide="next">' +
+    '<span class="carousel-control-next-icon" aria-hidden="true"></span>' +
+    '<span class="visually-hidden">Next</span>' +
+    '</button>' +
     '</div>';
-    $("#idSlider").empty();
-    $("#idSlider").append(item);
-    $("#vistamodal").modal('show');
+  $("#idSlider").empty();
+  $("#idSlider").append(item);
+  $("#vistamodal").modal('show');
 }
 
-function getItems(){
+function getItems() {
   let result = null;
   $.ajax({
-  url: "http://localhost:9000/api/todos",
-  type: "GET",
-  async: false,
-  cache: false,
-  success: function (d) {
-  listaDeProyecytos = d;
-  }
+    url: "http://localhost:9000/api/todos",
+    type: "GET",
+    async: false,
+    cache: false,
+    success: function (d) {
+      listaDeProyecytos = d;
+    }
   });
   return result;
-  }
-  
-  
+}
+
